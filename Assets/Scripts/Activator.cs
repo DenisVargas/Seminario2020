04ecb8e63173867f90ca57eee18ca3ca500f58ee
IgnacioIngold <43892862+IgnacioIngold@users.noreply.github.com>
2020-04-24 11:11:56 -0300

    jaula enemigo y mas

---
+++ Assets/Scripts/Activator.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Activator : MonoBehaviour, IInteractable
+{
+
+    public List<OperationOptions> suportedOperations = new List<OperationOptions>();
+    // Start is called before the first frame update
+    
+    
+
+    public Vector3 position { get => transform.position; }
+
+
+
+    public void Operate(OperationOptions operation, params object[] optionalParams)
+    {
+        Debug.LogWarning(string.Format("{0} se ha activado!", gameObject.name));
+        if (operation == OperationOptions.Activate)
+        {
+            GetComponentInChildren<Jail>().Drop();
+        }
+    }
+
+    private void Awake()
+    {
+       
+        
+    }
+
+    List<OperationOptions> IInteractable.GetSuportedOperations()
+    {
+        return suportedOperations;
+    }
+    
+
+}
---
+++ Assets/Scripts/Jail.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jail : MonoBehaviour
+{
+    Rigidbody _rb;
+    void Awake()
+    {
+        _rb = GetComponent<Rigidbody>();
+        _rb.isKinematic = true;
+    }
+
+    public void Drop()
+    {
+        _rb.isKinematic = false;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        var myhitedObject = collision as IDestructible;
+
+        if (myhitedObject != null)
+        {
+            myhitedObject.destroyMe();
+        }
+    }
+}

176cb52ec15738b36f3698be649baa600ffefd76
DenisVargasRivero <54225396+DenisVargasRivero@users.noreply.github.com>
2020-05-08 11:56:45 -0300

    Pequeños Ajustes

--- Assets/Scripts/Jail.cs
+++ Assets/Scripts/Jail.cs
@@ -1,10 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Core.DamageSystem;
 
+[RequireComponent(typeof(Collider))]
 public class Jail : MonoBehaviour
 {
+    [SerializeField] Collider PhysicalCollider;
+    [SerializeField] Collider _damageDealer;
     Rigidbody _rb;
+
     void Awake()
     {
         _rb = GetComponent<Rigidbody>();
@@ -14,6 +19,7 @@ public class Jail : MonoBehaviour
     public void Drop()
     {
         _rb.isKinematic = false;
+        StartCoroutine(Deactivate());
     }
 
     private void OnCollisionEnter(Collision collision)
@@ -24,5 +30,22 @@ public class Jail : MonoBehaviour
         {
             myhitedObject.destroyMe();
         }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        var killable = other.GetComponentInParent<IDamageable<Damage>>();
+        if (killable != null)
+        {
+            killable.Hit(new Damage() { instaKill = true });
+        }
+    }
+
+    IEnumerator Deactivate()
+    {
+        yield return new WaitForSeconds(10f);
+        PhysicalCollider.enabled = false;
+        yield return new WaitForSeconds(2f);
+        Destroy(gameObject);
     }
 }

c8ac1892bb8c947cb63c98a6ddf655f5176a2edc
DenisVargasRivero <54225396+DenisVargasRivero@users.noreply.github.com>
2020-05-20 12:27:26 -0300

    GroundTrigger + extras!
    En scripts se añadio una carpeta [Traps & Triggers] ya que ambos estan ligados!
    En Content hay una carpeta separada para ambas.
    En Scripts renombre [Gamplay] a [Traps] ya que movi la palanca a una nueva carpeta [Triggers] donde se agruparía con el GroundTrigger

---
+++ Assets/Scripts/Traps & Triggers/GroundTrigger.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+[RequireComponent(typeof(Collider))]
+public class GroundTrigger : MonoBehaviour
+{
+    UnityEvent OnActivate;
+    UnityEvent OnDeActivate;
+    //Action OnActivate = delegate { };
+    //Action OnDeActivate = delegate { };
+
+    [SerializeField] Animator _anims;
+    [SerializeField] float _deactivationTime;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        var player = other.GetComponent<NMA_Controller>();
+        if (player != null)
+        {
+            _anims.SetBool("Pressed", true);
+            OnActivate.Invoke();
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        var player = other.GetComponent<NMA_Controller>();
+        if (player != null)
+        {
+            _anims.SetBool("Pressed", false);
+            StartCoroutine(releaseActivation());
+        }
+    }
+
+    IEnumerator releaseActivation()
+    {
+        yield return new WaitForSeconds(_deactivationTime);
+        OnDeActivate.Invoke();
+    }
+}

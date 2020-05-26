757035971e28628e41df671ae9ccc253c00b9d24
IgnacioIngold <43892862+IgnacioIngold@users.noreply.github.com>
2020-05-26 13:19:52 -0300

    Clon movimiento y anims

---
+++ Assets/Content/player/jutsu/ClonBehaviour.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class ClonBehaviour : MonoBehaviour
+{
+
+    MouseContextTracker _mtracker;
+    Animator _anims;
+    NavMeshAgent _agent = null;
+    [SerializeField] float _movementTreshold = 0.18f;
+    Vector3 _currentTargetPos;
+
+    bool _a_Walking
+    {
+        get => _anims.GetBool("walking");
+        set => _anims.SetBool("walking", value);
+    }
+    // Start is called before the first frame update
+
+    void Awake()
+    {
+        _mtracker = GetComponent<MouseContextTracker>();
+        _anims = GetComponent<Animator>();
+        _agent = GetComponent<NavMeshAgent>();
+        
+    }
+   
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            MouseContext _mouseContext = _mtracker.GetCurrentMouseContext();
+            if (!_mouseContext.validHit) return;
+            Debug.Log("entre");
+            MoveToTarget(_mouseContext.hitPosition);
+
+        }
+        if (transform.position == _agent.destination)
+        {
+            _a_Walking = false;
+        }
+
+    }
+    public void MoveToTarget(Vector3 destinyPosition)
+    {
+        Vector3 _targetForward = (destinyPosition - transform.position).normalized.YComponent(0);
+        transform.forward = _targetForward;
+
+        if (!_a_Walking)
+            _a_Walking = true;
+
+        _agent.destination = destinyPosition;
+    }
+   
+
+
+}

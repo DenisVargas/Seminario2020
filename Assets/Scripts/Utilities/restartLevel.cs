8ac2712cd4691f86e0cc88d69c8b00e1c01c706c
DenisVargasRivero <54225396+DenisVargasRivero@users.noreply.github.com>
2020-06-05 15:21:57 -0300

    Update restartLevel.cs
    Ahora esto permite salir de la app apretando esc

--- Assets/Scripts/Utilities/restartLevel.cs
+++ Assets/Scripts/Utilities/restartLevel.cs
@@ -10,4 +10,19 @@ public class restartLevel : MonoBehaviour
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+#if UNITY_EDITOR
+            if(UnityEditor.EditorApplication.isPlaying)
+            {
+                UnityEditor.EditorApplication.isPlaying = false;
+            }
+            else
+#endif
+            Application.Quit();
+        }
+    }
 }

1c97478652bb9179ce3181bb20821eca4dd75348
DenisVargasRivero <54225396+DenisVargasRivero@users.noreply.github.com>
2020-08-17 12:31:18 -0300

    Custom Shorcuts!
    No puedo vivir sin el shorcut para bloquear el inspector!

---
+++ Assets/Editor/Utilities/CustomShorcuts.cs
@@ -0,0 +1,12 @@
+﻿using UnityEditor;
+
+[InitializeOnLoad]
+public class CustomShorcuts : Editor
+{
+    [MenuItem("Utility/Toogle Inspector Lock")]
+    public static void ToogleInspectorLock()
+    {
+        ActiveEditorTracker.sharedTracker.isLocked = !ActiveEditorTracker.sharedTracker.isLocked;
+        ActiveEditorTracker.sharedTracker.ForceRebuild();
+    }
+}

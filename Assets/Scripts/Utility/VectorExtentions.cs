cd575d94bfbdf28f907c811ee614946416ff0ada
DenisVargasRivero <54225396+DenisVargasRivero@users.noreply.github.com>
2020-08-17 12:27:54 -0300

    Merge Utilities to Utility
    Habían 2 carpetas con scripts similares, ahora los junte a uno solo :+1:

---
+++ Assets/Scripts/Utility/VectorExtentions.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+public static class VectorExtentions
+{
+    public static Vector3 RandomVector3(this Vector3 Vector, int min = 0, int max = 2)
+    {
+        Vector = new Vector3(Random.Range(min, max), Random.Range(min, max), Random.Range(min, max));
+        return Vector;
+    }
+    public static Vector3 RandomVector3(this Vector3 Vector, float min = 0f, float max = 1f)
+    {
+        Vector = new Vector3(Random.Range(min, max), Random.Range(min, max), Random.Range(min, max));
+        return Vector;
+    }
+    public static Vector3 getScaled(this Vector3 vector, Vector3 scale)
+    {
+        return new Vector3(vector.x * scale.x, vector.y * scale.y, vector.z * scale.z);
+    }
+    public static Vector3 DirTo(this Vector3 origin, Vector3 targetPosition)
+    {
+        return (targetPosition - origin).normalized;
+    }
+    public static Vector3 YComponent(this Vector3 vector, float value)
+    {
+        return new Vector3(vector.x, value, vector.z);
+    }
+    public static Vector3 XComponent(this Vector3 vector, float value)
+    {
+        return new Vector3(value, vector.y, vector.z);
+    }
+    public static Vector3 ZComponent(this Vector3 vector, float value)
+    {
+        return new Vector3(vector.x, vector.y, value);
+    }
+}

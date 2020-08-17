6c038275bacebf86966c3a1c0d480715163cc316
DenisVargasRivero <54225396+DenisVargasRivero@users.noreply.github.com>
2020-08-17 12:26:04 -0300

    Node Based Navigation System P1
    Añadido los nodos de Pathfing.
    TODO: herramienta para acelerar la creación de los nodos de navegación!, un Level Editor no sería mala idea.

---
+++ Assets/Scripts/Navigation/Node.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace IA.PathFinding
+{
+    public enum NavigationArea
+    {
+        nonNavegable = 0,
+        Navegable
+    }
+
+    public class Node : MonoBehaviour
+    {
+        public int ID = 0;      //ID del nodo dentro de una Graph.
+        public List<Node> Connections; //Referencias a los nodos a los que estoy conectado, usado por PathFinding.
+        public int area = 0; //Esto lo vamos a usar para detectar si es navegable o no.
+
+    #if UNITY_EDITOR
+        private void OnDrawGizmos()
+        {
+            //Esto es horrible por si las dudas!
+            if (Connections == null) return;
+
+            foreach (var node in Connections)
+            {
+                if (node.Connections.Contains(this))
+                {
+                    Gizmos.color = Color.green;
+                    Gizmos.DrawLine(this.transform.position, node.transform.position);
+                }
+            }
+        }
+    #endif
+    }
+}
+

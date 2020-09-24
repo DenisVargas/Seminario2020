d66b8668ceeb9b0710cd35110c6cc89aab780da1
IgnacioIngold <43892862+IgnacioIngold@users.noreply.github.com>
2020-09-24 17:48:36 -0300

    UI mejorada

--- Assets/Scripts/UI/CommandMenu/CommandMenuItem.cs
+++ Assets/Scripts/UI/CommandMenu/CommandMenuItem.cs
@@ -18,7 +18,10 @@ public class CommandMenuItem : MonoBehaviour
     //Componentes que queremos modificar.
     [SerializeField] TMP_Text _text = null;
     [SerializeField] Image _iconImageField = null;
-    [SerializeField] RawImage _backGroundImage = null;
+    [SerializeField] Image _backGroundImage = null;
+    public Sprite normalState;
+    public Sprite onHover;
+    public Sprite onPress;
 
     [SerializeField] Color _normalColor  = Color.white;
     [SerializeField] Color _hoverColor   = Color.white;
@@ -36,20 +39,24 @@ public class CommandMenuItem : MonoBehaviour
                 _text.SetText(data.CommandName);
                 _iconImageField.sprite = data.Icon;
             }
-            _backGroundImage.color = _normalColor;
+            //_backGroundImage.color = _normalColor;
         }
     }
 
     public void OnMouseHoverStart()
     {
         //Si ponemos el mouse encima.
-        _backGroundImage.color = _hoverColor;
+        //_backGroundImage.color = _hoverColor;
+        _backGroundImage.sprite = onHover;
+        _text.color = Color.white;
+        
     }
     public void OnMOuseClickDown()
     {
         //Si hacemos clic.
-        _backGroundImage.color = _pressedColor;
+        //_backGroundImage.color = _pressedColor;
         //Ejecutamos nuestro delegado.
+        _backGroundImage.sprite = onPress;
         OnOperationSelected(data.Operation, referenceComponent);
     }
     public void OnMouseClickUp()
@@ -59,6 +66,8 @@ public class CommandMenuItem : MonoBehaviour
     public void OnMouseHoverEnd()
     {
         //Si sacamos el mouse de encima.
-        _backGroundImage.color = _normalColor;
+        //_backGroundImage.color = _normalColor;
+        _backGroundImage.sprite = normalState;
+        _text.color = Color.black;
     }
 }

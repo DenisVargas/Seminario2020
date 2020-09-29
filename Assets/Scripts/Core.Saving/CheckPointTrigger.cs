1dca14d1a638c1bff2ca96210f8be46da4fb41b8
DenisVargasRivero <54225396+DenisVargasRivero@users.noreply.github.com>
2020-09-29 18:59:27 -0300

    Reorder Prefabs + Add Checkpoints in Scene

---
+++ Assets/Scripts/Core.Saving/CheckPointTrigger.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using Core.SaveSystem;
+
+public class CheckPointTrigger : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            //Debug.Log("CheckPointAdded");
+            //Me aseguro que el checkpoint solo se añada si el jugador esta vivo en el momento del checkPoint.
+            var controller = other.GetComponent<Controller>();
+            if (controller.IsAlive)
+                Level.SetCheckPoint();
+        }
+    }
+}

5e4bdbb1261bd64414792123becd08957c605b55
IgnacioIngold <43892862+IgnacioIngold@users.noreply.github.com>
2020-10-27 03:24:47 -0300

    pause menu

---
+++ Assets/Scripts/Items/fire.cs
@@ -0,0 +1,25 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Core.DamageSystem;
+
+public class fire : MonoBehaviour
+{
+    public Damage MyDamage;
+    // Start is called before the first frame update
+   
+
+    // Update is called once per frame
+   
+    private void OnCollisionEnter(Collision collision)
+    {
+        var damagecomponent = collision.gameObject.GetComponent<IDamageable<Damage, HitResult>>();
+        if (damagecomponent != null)
+        {
+            Damage enemyCollisionDamage = new Damage() { type = DamageType.hit };
+            damagecomponent.GetHit(MyDamage); //Causo daño por choque.
+        }
+    }
+
+}

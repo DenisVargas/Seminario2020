f8b7148b219feee1506979c083fdea6ffabd1c99
IgnacioIngold <43892862+IgnacioIngold@users.noreply.github.com>
2020-10-30 10:36:25 -0300

    lvl1 y tutorial

--- Assets/Scripts/Items/fire.cs
+++ Assets/Scripts/Items/fire.cs
@@ -8,18 +8,24 @@ public class fire : MonoBehaviour
 {
     public Damage MyDamage;
     // Start is called before the first frame update
-   
+
 
     // Update is called once per frame
-   
-    private void OnCollisionEnter(Collision collision)
+
+    //private void OnCollisionEnter(Collision collision)
+    //{
+    //    var damagecomponent = collision.gameObject.GetComponent<IDamageable<Damage, HitResult>>();
+    //    if (damagecomponent != null)
+    //    {
+    //        Damage enemyCollisionDamage = new Damage() { type = DamageType.hit };
+    //        damagecomponent.GetHit(MyDamage); //Causo daño por choque.
+    //    }
+    //}
+    private void OnTriggerStay(Collider other)
     {
-        var damagecomponent = collision.gameObject.GetComponent<IDamageable<Damage, HitResult>>();
-        if (damagecomponent != null)
-        {
-            Damage enemyCollisionDamage = new Damage() { type = DamageType.hit };
-            damagecomponent.GetHit(MyDamage); //Causo daño por choque.
-        }
+        var damageable = other.GetComponent<IDamageable<Damage, HitResult>>();
+        if (damageable != null)
+            damageable.GetHit(MyDamage);
     }
 
 }

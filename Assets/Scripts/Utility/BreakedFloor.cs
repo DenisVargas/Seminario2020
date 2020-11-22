d0d9294f4cf3fd42a473c35645fb3cff16d095f2
IgnacioIngold <luis.ingold@davinci.edu.ar>
2020-11-22 16:10:22 -0300

    cambios prefab luz, altar y piso

---
+++ Assets/Scripts/Utility/BreakedFloor.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BreakedFloor : MonoBehaviour
+{
+    public List<Rigidbody> pieces = new List<Rigidbody>();
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            foreach (var item in pieces)
+            {
+                item.useGravity = true;
+            }
+          
+        }
+        Debug.Log("entre a la collision");
+        
+    }
+}
